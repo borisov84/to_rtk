63f9f8b6aec21541ca46a82c6e0dac902a8ea132
Borisov84 <toxa100500@gmail.com>
2023-02-26 16:21:08 +0500

    Making 'otsev'

--- convert_csv.py
+++ convert_csv.py
@@ -10,18 +10,21 @@ departments_files = os.listdir('departments')
 # print(departments_files)
 wb = openpyxl.Workbook()
 os.chdir('departments')
+# Делаем словарь из филиалов и номеров телефонов
 for df in departments_files:
     with open(df, 'r') as file:
         # print(file.name)
         phone_numbers = file.read().splitlines()
         departments_numbers[file.name] = phone_numbers
         sheet = wb.create_sheet(file.name.split('.')[0])
-print(departments_numbers)
+# print(departments_numbers)
+
 
 def convert():
     os.chdir('..')
     filename = 'rtk.csv'
     result = []
+    # перебираем файл и собираем звонки в список
     with open(filename, 'r', encoding='utf-8') as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
@@ -36,19 +39,32 @@ def convert():
     # print(len(result))
     # print(result)
     csv_file.close()
-    row = 1
-    for sums in result:
+    # перебираем звонки и распределяем по филиам
+    # заполняем таблицу Excel по филиалам
+    to_remove = []
+    for ind, sums in enumerate(result):
         for dep, nums in departments_numbers.items():
             # print(f'Sums - {sums}, Nums - {nums}')
             if sums[0] in nums:
-                print(f'{dep} {sums[0]}: {sums[1]}')
+                # print(f'{dep} {sums[0]}: {sums[1]}')
                 sheet = wb[dep.split('.')[0]]
                 num_cell = sheet.cell(row=sheet.max_row + 1, column=1)
                 num_cell.value = sums[0]
                 money_cell = sheet.cell(row=sheet.max_row, column=2)
                 money_cell.value = sums[1]
-                row += 1
+                to_remove.append(ind)
+    print(len(to_remove))
+    result[:] = [x for i,x in enumerate(result) if i not in to_remove]
+    # выводим отсев
+    sheet = wb['Sheet']
+    sheet.title = 'Отсев'
+    for o in result:
+        ots_cell = sheet.cell(row=sheet.max_row+1, column=1)
+        ots_cell.value = o[0]
+        ots_cell = sheet.cell(row=sheet.max_row, column=2)
+        ots_cell.value = o[1]
     wb.save('output.xlsx')
+    print(len(result))
 
 
 if __name__ == '__main__':
